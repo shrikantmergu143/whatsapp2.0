ae0e7530a1be386fee490175333dba3cc5478239
Shriakant Narsayya Mergu <69249035+shrikantmergu143@users.noreply.github.com>
2022-12-06 16:14:20 +0530

    Update environment.js

--- environments/environment.js
+++ environments/environment.js
@@ -1,12 +1,13 @@
 const environment = {
   firebaseConfig: {
-  apiKey: "AIzaSyA455IScPVXoVAjMP8Kjpogbc2rVM_u0WA",
-  authDomain: "instashre-2c6a1.firebaseapp.com",
-  databaseURL: "https://instashre-2c6a1-default-rtdb.firebaseio.com",
-  projectId: "instashre-2c6a1",
-  storageBucket: "instashre-2c6a1.appspot.com",
-  messagingSenderId: "758145740018",
-  appId: "1:758145740018:web:cfebe0d0d2b43bef61346b"
+    apiKey: "AIzaSyA455IScPVXoVAjMP8Kjpogbc2rVM_u0WA",
+    authDomain: "instashre-2c6a1.firebaseapp.com",
+    databaseURL: "https://instashre-2c6a1-default-rtdb.firebaseio.com",
+    projectId: "instashre-2c6a1",
+    storageBucket: "instashre-2c6a1.appspot.com",
+    messagingSenderId: "758145740018",
+    appId: "1:758145740018:web:aee92e0bc9870e5161346b",
+    measurementId: "G-P01S3D0MBH"
   },
 };
 
