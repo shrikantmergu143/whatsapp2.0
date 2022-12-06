05e8be9907678c219ac91b66762d04cd3e8adff8
Shriakant Narsayya Mergu <69249035+shrikantmergu143@users.noreply.github.com>
2022-12-06 15:57:13 +0530

    Update environment.template.js

--- environments/environment.template.js
+++ environments/environment.template.js
@@ -1,5 +1,13 @@
 const environment = {
-  firebaseConfig: {},
+  firebaseConfig: {
+  apiKey: "AIzaSyA455IScPVXoVAjMP8Kjpogbc2rVM_u0WA",
+  authDomain: "instashre-2c6a1.firebaseapp.com",
+  databaseURL: "https://instashre-2c6a1-default-rtdb.firebaseio.com",
+  projectId: "instashre-2c6a1",
+  storageBucket: "instashre-2c6a1.appspot.com",
+  messagingSenderId: "758145740018",
+  appId: "1:758145740018:web:cfebe0d0d2b43bef61346b"
+  },
 };
 
 export default environment;
